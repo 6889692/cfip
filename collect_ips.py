94265d67e0c24a2286355fa47f6a3bd6fa7e8e93
fjvi <fjvi@users.noreply.github.com>
2025-05-10 15:19:10 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -5,7 +5,8 @@ import os
 
 # 目标URL列表
 urls = ['https://www.wetest.vip/page/cloudflare/address_v4.html', 
-        'https://ip.164746.xyz'
+        'https://ip.164746.xyz' 
+        'https://raw.githubusercontent.com/indianajson/cloudflare-nameservers/main/cloudflare-ips.txt'        
         ]
 
 # 正则表达式用于匹配IP地址
@@ -28,6 +29,8 @@ with open('ip.txt', 'w') as file:
         if url == 'https://www.wetest.vip/page/cloudflare/address_v4.html':
             elements = soup.find_all('tr')
         elif url == 'https://ip.164746.xyz':
+            elements = soup.find_all('tr')
+        elif url == 'https://raw.githubusercontent.com/indianajson/cloudflare-nameservers/main/cloudflare-ips.txt':
             elements = soup.find_all('tr')
         else:
             elements = soup.find_all('li')
