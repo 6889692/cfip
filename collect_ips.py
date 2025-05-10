d872b884d954df8c98711fdcc327f2ddea735558
fjvi <fjvi@users.noreply.github.com>
2025-05-10 15:20:31 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -5,7 +5,7 @@ import os
 
 # 目标URL列表
 urls = ['https://www.wetest.vip/page/cloudflare/address_v4.html', 
-        'https://ip.164746.xyz' 
+        'https://ip.164746.xyz',
         'https://raw.githubusercontent.com/indianajson/cloudflare-nameservers/main/cloudflare-ips.txt'        
         ]
 
